c8108aaaacc0c5594867c08f80b57d30e78faef7
tech2go-web <xianshao351@gmail.com>
2024-12-24 00:53:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -108,6 +108,6 @@ function switchTheme(theme) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  const savedTheme = localStorage.getItem('theme') || 'light';
+  const savedTheme = localStorage.getItem('theme') || 'dark';
   switchTheme(savedTheme);
 });
